93860cccad6de22af4bde348f256e51e7ca43a0d
Tangyonghai <tech@geiqin.com>
2021-07-11 10:53:54 +0800

    big modify

--- uim/tenants/managementService.pb.micro.go
+++ uim/tenants/managementService.pb.micro.go
@@ -4,7 +4,7 @@
 package services
 
 import (
-	common "github.com/geiqin/microkit/protobuf/common"
+	common "github.com/geiqin/micro-kit/protobuf/common"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
 	math "math"
