ea9fc50e4912778540ccde3f17596f43ddbfc34d
Tangyonghai <tech@geiqin.com>
2022-02-10 15:24:08 +0800

    modify media

--- cms/media/tagService.pb.go
+++ cms/media/tagService.pb.go
@@ -32,9 +32,9 @@ type Tag struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Id       int32  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	Name     string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
-	TotalNum int32  `protobuf:"varint,5,opt,name=total_num,json=totalNum,proto3" json:"total_num,omitempty"`
+	Id       int32  `protobuf:"varint,1,opt,name=id,proto3" json:"id"`
+	Name     string `protobuf:"bytes,2,opt,name=name,proto3" json:"name"`
+	TotalNum int32  `protobuf:"varint,5,opt,name=total_num,json=totalNum,proto3" json:"total_num"`
 }
 
 func (x *Tag) Reset() {
@@ -96,11 +96,11 @@ type TagResponse struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Entity *Tag          `protobuf:"bytes,1,opt,name=entity,proto3" json:"entity,omitempty"`
-	Pager  *common.Pager `protobuf:"bytes,2,opt,name=pager,proto3" json:"pager,omitempty"`
-	Items  []*Tag        `protobuf:"bytes,3,rep,name=items,proto3" json:"items,omitempty"`
-	Error  *common.Error `protobuf:"bytes,4,opt,name=error,proto3" json:"error,omitempty"`
-	Info   *common.Info  `protobuf:"bytes,5,opt,name=info,proto3" json:"info,omitempty"`
+	Entity *Tag          `protobuf:"bytes,1,opt,name=entity,proto3" json:"entity"`
+	Pager  *common.Pager `protobuf:"bytes,2,opt,name=pager,proto3" json:"pager"`
+	Items  []*Tag        `protobuf:"bytes,3,rep,name=items,proto3" json:"items"`
+	Error  *common.Error `protobuf:"bytes,4,opt,name=error,proto3" json:"error"`
+	Info   *common.Info  `protobuf:"bytes,5,opt,name=info,proto3" json:"info"`
 }
 
 func (x *TagResponse) Reset() {
